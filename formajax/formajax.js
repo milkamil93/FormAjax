4bfcc5a7cc6cbac4e69a4c0266167a4452c40f6b
milkamil93 <milkamil93@gmail.com>
2019-11-25 11:15:54 +0500

    update

--- formajax/formajax.js
+++ formajax/formajax.js
@@ -6,27 +6,30 @@
  */
 
 ;(function () {
-    var $settings = {
+    var settings = {
         empty: 'Заполните необходимые поля',
         fatal: 'Неизвестная ошибка',
         sending: 'Отправка сообщения',
-        requestpage: 'Страница с запросом',
+        requestPage: 'Страница с запросом',
         referrer: 'Источник трафика',
         sent: 'Сообщение отправлено',
-        yandexMetrika: function (TARGET) {
+        callback: function (form) {
 
-            // старая метрика
-            //yaCounter47027148.reachGoal(TARGET);
+            /*var target;
+            if ((target = form.getAttribute('data-target'))) {
+                // старая метрика
+                //yaCounter47027148.reachGoal(target);
 
-            // новая метрика
-            //ym(47027148, 'reachGoal', TARGET);
+                // новая метрика
+                //ym(47027148, 'reachGoal', target);
+            }*/
         }
     };
 
-    var $formajax = {
-        init: function ($form) {
-            this.form = $form;
-            this.settings = $settings;
+    var formAjax = {
+        init: function (form) {
+            this.form = form;
+            this.settings = settings;
             this.typefileds = 'input:not([type="button"]):not([type="image"]):not([type="password"]):not([type="reset"]):not([type="submit"]),textarea,select';
             this.names = {};
             this.error = false;
@@ -46,76 +49,76 @@
         },
 
         /* Вывести статус формы */
-        statusForm: function ($text, $status) {
-            if ($text) {
-                $status = $status || 'warning';
-                var $alert = this.form.getElementsByClassName('alert');
-                if ($alert.length) {
-                    $alert[0].setAttribute('class', 'alert alert-' + $status);
-                    $alert[0].textContent = $text;
+        statusForm: function (text, status) {
+            if (text) {
+                status = status || 'warning';
+                var alert = this.form.getElementsByClassName('alert');
+                if (alert.length) {
+                    alert[0].setAttribute('class', 'alert alert-' + status);
+                    alert[0].textContent = text;
                 } else {
-                    this.form.insertBefore(createEl('div','alert alert-' + $status, $text), this.form.firstChild);
+                    this.form.insertBefore(createEl('div','alert alert-' + status, text), this.form.firstChild);
                 }
 
             }
         },
 
         /* Вывести ошибку поля */
-        statusField: function ($el) {
+        statusField: function (el) {
             this.error = true;
-            var $msg = $el.getAttribute('data-error') || 'Поле обязательно для заполнения';
-            var inputInvalid = document.createElement('div');
+            var msg = el.getAttribute('data-error') || 'Поле обязательно для заполнения',
+                inputInvalid = document.createElement('div');
             inputInvalid.className = 'invalid-feedback';
-            inputInvalid.innerText = $msg;
-            $el.after(inputInvalid);
+            inputInvalid.innerText = msg;
+            el.after(inputInvalid);
             inputInvalid.addEventListener('mouseover', () => {
-                $el.classList.remove('is-invalid');
-                $el.focus();
+                el.classList.remove('is-invalid');
+                el.focus();
                 inputInvalid.remove();
             });
         },
 
         /* Проверка полей на валидность, подготовка формы к отправке */
         checkFields: function () {
-            var $fields = this.form.querySelectorAll(this.typefileds),
-                $allRequired = this.form.querySelectorAll('[required]').length,
-                $this = this,
-                $cr = {
+            var fields = this.form.querySelectorAll(this.typefileds),
+                allRequired = this.form.querySelectorAll('[required]').length,
+                self = this,
+                cr = {
                     sets: {},
                     status: {}
                 };
-            $fields.forEach(function ($item) {
-                var $name = $item.getAttribute('name'),
-                    $type = $item.getAttribute('type');
-                var $next = $item.nextElementSibling;
-                if ($next && is($item.nextElementSibling, '.invalid-feedback')) {
-                    $next.parentNode.removeChild($next);
-                    removeClass($item, 'is-invalid');
+            fields.forEach(function (item) {
+                var name = item.getAttribute('name'),
+                    type = item.getAttribute('type'),
+                    next = item.nextElementSibling;
+                if (next && is(item.nextElementSibling, '.invalid-feedback')) {
+                    next.parentNode.removeChild(next);
+                    removeClass(item, 'is-invalid');
                 }
-                if (!$allRequired || $item.getAttribute('required') !== null) {
+                if (!allRequired || item.getAttribute('required') !== null) {
 
                     /* Сбор ошибок */
-                    switch ($type) {
+                    switch (type) {
 
                         /* Проверка checkbox и radio полей на выбранность */
                         case 'checkbox':
                         case 'radio': {
-                            $cr.sets[$name] = $cr.sets[$name] === undefined ? $this.form.querySelectorAll('[name="' + $name + '"]') : $cr.sets[$name];
-                            $cr.status[$name] = $cr.status[$name] ? $cr.status[$name] : $item.checked;
-                            if ($cr.sets[$name].length === indexSET($cr.sets[$name], $item)) {
-                                if (!$cr.status[$name]) {
-                                    addClass($cr.sets[$name], 'is-invalid');
-                                    $this.statusField($item);
+                            cr.sets[name] = cr.sets[name] === undefined ? self.form.querySelectorAll('[name="' + name + '"]') : cr.sets[name];
+                            cr.status[name] = cr.status[name] ? cr.status[name] : item.checked;
+                            if (cr.sets[name].length === indexSET(cr.sets[name], item)) {
+                                if (!cr.status[name]) {
+                                    addClass(cr.sets[name], 'is-invalid');
+                                    self.statusField(item);
                                 } else {
-                                    removeClass($cr.sets[$name], 'is-invalid');
+                                    removeClass(cr.sets[name], 'is-invalid');
                                 }
                             }
                             break;
                         }
                         default: {
-                            if (!$item.value.length) {
-                                addClass($item, 'is-invalid');
-                                $this.statusField($item);
+                            if (!item.value.length) {
+                                addClass(item, 'is-invalid');
+                                self.statusField(item);
                             }
                         }
                     }
@@ -123,79 +126,75 @@
                 }
 
                 /* Сбор имён */
-                var $dataname;
-                if (($dataname = $item.getAttribute('data-name'))) {
-                    $this.names[$name] = $dataname;
+                var names;
+                if ((names = item.getAttribute('data-name'))) {
+                    self.names[name] = names;
                 }
             });
         },
 
         /* Функция отправки */
         send: function () {
-            var $this = this;
-            $this.checkFields();
-            if($this.error) return false;
+            var self = this;
+            self.checkFields();
+            if(self.error) return false;
 
-            var $formData = new FormData($this.form),
-                $toDelete = [];
+            var formData = new FormData(self.form),
+                toDelete = [];
 
             /* Удаляем пустые поля */
-            $formData.forEach(function ($value, $key) {
-                if (typeof $value === 'object' && $value.size === 0 || $value.length === 0) {
-                    $toDelete.push($key);
+            formData.forEach(function (value, key) {
+                if (typeof value === 'object' && value.size === 0 || value.length === 0) {
+                    toDelete.push(key);
                 }
-                if (typeof $value === 'object' && $value.size >= 10485760) { // 10 МБ
-                    $this.statusForm('Слишком большой размер файла', 'danger');
-                    $this.error = true;
+                if (typeof value === 'object' && value.size >= 10485760) { // 10 МБ
+                    self.statusForm('Слишком большой размер файла', 'danger');
+                    self.error = true;
                 }
             });
-            $toDelete.forEach(function ($value) {
-                $formData.delete($value);
+            toDelete.forEach(function (value) {
+                formData.delete(value);
             });
 
             /* Источник трафика и страница с запросом */
-            $this.names.fa_requestpage = $settings.requestpage;
-            $this.names.fa_referrer = $settings.referrer;
-            $formData.append('fa_requestpage', location.href);
-            $formData.append('fa_referrer', $this.getReferrer());
+            self.names.fa_requestpage = settings.requestPage;
+            self.names.fa_referrer = settings.referrer;
+            formData.append('fa_requestpage', location.href);
+            formData.append('fa_referrer', self.getReferrer());
 
             /* Набор идентификаторов */
-            $formData.append('fa_names', JSON.stringify($this.names));
+            formData.append('fa_names', JSON.stringify(self.names));
 
             /* Индивидульная тема формы */
-            var $subject;
-            if (($subject = $this.form.getAttribute('data-formajax'))) {
-                $formData.append('fa_subject', $subject);
+            var subject;
+            if ((subject = self.form.getAttribute('data-formajax'))) {
+                formData.append('fa_subject', subject);
             }
 
             /* Индивидульный получатель формы */
             var $to;
-            if (($to = $this.form.getAttribute('data-to'))) {
-                $formData.append('fa_to', $to);
+            if (($to = self.form.getAttribute('data-to'))) {
+                formData.append('fa_to', $to);
             }
 
-            if($this.error) return false;
+            if(self.error) return false;
 
 
-            $this.statusForm($settings.sending, 'warning');
+            self.statusForm(settings.sending, 'warning');
 
             /* Делаем запрос */
-            var $request = new XMLHttpRequest();
-            $request.open('POST', 'XMLHttpRequestForm', true);
-            $request.setRequestHeader('X-REQUESTED-WITH', 'FormAjaxRequest');
-            $request.onload = function() {
-                var $resp = JSON.parse($request.responseText);
-                if ($request.status >= 200 && $request.status < 400) {
+            var request = new XMLHttpRequest();
+            request.open('POST', '__ajax__/form', true);
+            request.setRequestHeader('X-REQUESTED-WITH', 'FormAjaxRequest');
+            request.onload = function() {
+                var response = JSON.parse(request.responseText);
+                if (request.status >= 200 && request.status < 400) {
 
                     /* Результат успешного запроса */
-                    var $type;
-                    if ($resp.status) {
-                        $type = 'success';
-                        $this.form.querySelectorAll('button,[type="button"],[type="submit"]')[0].setAttribute('disabled', '');
-                        var $target;
-                        if (($target = $this.form.getAttribute('data-target'))) {
-                            $this.settings.yandexMetrika($target);
-                        }
+                    var type;
+                    if (response.status) {
+                        type = 'success';
+                        self.form.querySelectorAll('button,[type="button"],[type="submit"]')[0].setAttribute('disabled', '');
 
                         /* Закрытие popup */
                         if (typeof($) !== 'undefined') {
@@ -212,52 +211,55 @@
                                 }
                             }, 2000);
                         }
+
+                        /* Функция после успешной отправки */
+                        self.settings.callback(self.form);
                     } else {
-                        $type = 'danger';
+                        type = 'danger';
                     }
-                    $this.statusForm($resp.messages, $type);
+                    self.statusForm(response.messages, type);
                 } else {
 
                     /* Ошибка запроса */
-                    $this.statusForm($resp, 'danger');
+                    self.statusForm(response, 'danger');
                 }
             };
-            $request.onerror = function($error) {
+            request.onerror = function(error) {
 
                 /* Прочие ошибки */
-                $this.statusForm($error.type, 'danger');
+                self.statusForm(error.type, 'danger');
             };
-            $request.send($formData);
+            request.send(formData);
         }
     };
 
     /* Ловим события отправки формы */
     addEvent(document, 'submit', '[data-formajax]', function(e) {
         e.preventDefault();
-        $formajax.init(this);
+        formAjax.init(this);
     });
 
     /* Записываем источник трафика */
-    window.onload = $formajax.setReferrer(this);
+    window.onload = formAjax.setReferrer(this);
 
     /* Отключаем стандартную валидацию HTML5 у наших форм */
     var DOMEvents = ['DOMSubtreeModified','DOMContentLoaded'];
     for (var i = 0; i < DOMEvents.length; i++) {
         document.addEventListener(DOMEvents[i], function() {
-            document.querySelectorAll('[data-formajax]:not([novalidate])').forEach(function ($item) {
-                $item.setAttribute('novalidate', '');
+            document.querySelectorAll('[data-formajax]:not([novalidate])').forEach(function (item) {
+                item.setAttribute('novalidate', '');
             });
         });
     }
 
-    function indexSET($set, $el) {
-        var $number = 0;
-        $set.forEach(function ($item, i) {
-            if ($item === $el) {
-                $number = ++i;
+    function indexSET(set, el) {
+        var number = 0;
+        set.forEach(function (item, i) {
+            if (item === el) {
+                number = ++i;
             } else return true;
         });
-        return $number;
+        return number;
     }
 
     function createEl(tag, className, text) {
@@ -269,8 +271,8 @@
 
     function addClass(el, className) {
         if (el.length) {
-            el.forEach(function ($item) {
-                addCl($item);
+            el.forEach(function (item) {
+                addCl(item);
             });
         } else {
             addCl(el);
@@ -286,8 +288,8 @@
 
     function removeClass(el, className) {
         if (el.length) {
-            el.forEach(function ($item) {
-                removeCl($item);
+            el.forEach(function (item) {
+                removeCl(item);
             });
         } else {
             removeCl(el);
@@ -302,7 +304,7 @@
 
     }
 
-    var is = function(el, selector) {
+    function is(el, selector) {
         return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
     };
 
